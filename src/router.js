45d70d206694c59ecfd62704b0f3bd24542c0588
Taisei Nakajima <ne300085@senshu-u.jp>
2019-10-14 12:39:33 +0900

    edit route && create views

--- src/router.js
+++ src/router.js
@@ -1,27 +1,44 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
 
 Vue.use(Router)
+
+function loadView (view) {
+  return () => import(/* webpackChunkName: "view-[request]" */ `@/views/${view}.vue`)
+}
 
 export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
+    // {
+    //   path: '/',
+    //   name: 'home',
+    //   component: loadView('Home')
+    // },
+    // {
+    //   path: '/about',
+    //   name: 'about',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (about.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: loadView('About')
+    // },
     {
-      path: '/',
-      name: 'home',
-      component: Home
+      path: '/result',
+      name: 'result',
+
+      component: loadView('Result')
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: function () { 
-        return import(/* webpackChunkName: "about" */ './views/About.vue')
-      }
+      path: '/ranking',
+      name: 'ranking',
+      component: loadView('Ranking')
+    },
+    {
+      path: '/graph',
+      name: 'graph',
+      component: loadView('Graph')
     }
   ]
 })
