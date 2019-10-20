1132e37ef829377d783ffbeb1fa570bbea56491d
Taisei Nakajima <ne300085@senshu-u.jp>
2019-10-20 15:53:18 +0900

    データ表示とデータ管理の準備

--- src/views/Result.vue
+++ src/views/Result.vue
@@ -1,13 +1,40 @@
 <template>
   <div class="result">
-
+    <p>name:{{ name }}</p>
+    <p>score:{{ score }}</p>
+    <p>life:{{ life }}</p>
+    <p>muscle:{{ muscle }}</p>
   </div>
 </template>
 
 <script>
+  import axios from 'axios'
   export default {
-    name: "result"
-  }
+    name: 'result',
+    props: {
+      id: Number
+    },
+    data() {
+      return {
+        name: Text,
+        score: Number,
+        life: Number,
+        muscle: Number,
+        res: null
+
+      }
+    },
+    created() {
+      axios.get('https://muscle-horror-api.herokuapp.com/results/' + this.id)
+        .then(response => {
+          this.name = response.data.result.name;
+          this.score = response.data.result.score;
+          this.life = response.data.result.life;
+          this.muscle = response.data.result.muscle;
+          this.res = response;
+        })
+    }
+  };
 </script>
 
 <style scoped>
