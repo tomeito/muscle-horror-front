9dc85b17e7cebca28e54abd85759f0f672bddcba
Taisei Nakajima <ne300085@senshu-u.jp>
2019-10-26 01:30:14 +0900

    add radar.vue

---
+++ src/components/radar.vue
@@ -0,0 +1,28 @@
+<template>
+  
+</template>
+
+<script>
+  import { Radar } from 'vue-chartjs'
+
+  export default {
+    extends: Radar,
+    name: "radar",
+    data() {
+      return {
+        label: ["筋肉量", "素早さ", "相性", "探査能力", "ライフ"],
+        datasets: [
+          {
+            data: [0, 0, 0, 0, 0],
+            backgroundColor: "rgba(255, 0, 92, 0.3)",
+            borderColor: "rgba(255, 0, 92, 1)",
+          }
+        ],
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
